39e3457bd33ad4d1746a462bd0a8933f78ac67c6
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-02-10 14:45:17 +0500

    add solution for task2

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,24 +4,27 @@
   44 5 78 -> 78
   22 3 9 -> 22 */
 
-int[] array1 = new int[]{2, 3, 7};
-int[] array2 = new int[]{44, 5, 78};
-int[] array3 = new int[]{22, 3, 9};
+int[] array1 = new int[] { 2, 3, 7 };
+int[] array2 = new int[] { 44, 5, 78 };
+int[] array3 = new int[] { 22, 3, 9 };
 int max1 = 0, items1 = array1.Length;
 int max2 = 0, items2 = array2.Length;
 int max3 = 0, items3 = array3.Length;
 
-for (int i = 0; i < items1; i++) {
+for (int i = 0; i < items1; i++)
+{
   if (array1[i] > max1) max1 = array1[i];
 }
 Console.WriteLine("Max number of 2, 3, 7 is " + max1);
 
-for (int i = 0; i < items2; i++) {
+for (int i = 0; i < items2; i++)
+{
   if (array2[i] > max2) max2 = array2[i];
 }
 Console.WriteLine("Max number of 44, 5, 78 is " + max2);
 
-for (int i = 0; i < items3; i++) {
+for (int i = 0; i < items3; i++)
+{
   if (array3[i] > max3) max3 = array3[i];
 }
 Console.WriteLine("Max number of 22, 3, 9 is " + max3);
---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+(делится ли оно на два без остатка).
+
+  4 -> да
+  3 -> нет
+  7 -> нет */
+
+int[] array = new int[] { 4, 3, 7 };
+int items = array.Length;
+
+for (int i = 0; i < items; i++)
+{
+  if (array[i] % 2 == 0) Console.WriteLine("Even number of 4, 3, 7: " + array[i]);
+  else Console.WriteLine("Odd number of 4, 3, 7: " + array[i]);
+}
