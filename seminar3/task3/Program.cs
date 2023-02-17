a1d64e875781adfbe43c4524b9a920684c31caed
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-02-17 20:30:47 +0500

    added task3 for sem3 (1st solution)

---
+++ seminar3/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+* при N < 0, нужно вывести от N до -1
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.Clear();
+
+Console.Write("Enter a number: ");
+int N = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i < N; i++)
+{
+  double result = Math.Pow(i, 3);
+  Console.Write($"{N} -> {result} | "); 
+} 
