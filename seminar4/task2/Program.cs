6e10b565135bbfab6961fb13ca2a1c2221005d0d
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-02-23 20:05:36 +0500

    added task2 and task3 for sem4

---
+++ seminar4/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.Clear();
+Console.Write("Enter a number: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{num} -> {GetNumberSum(num)}");
+
+int GetNumberSum(int nums)
+{
+  int result = 0;
+  while (nums > 0) 
+  {
+    result += nums % 10;
+    nums /= 10;
+  }
+  return result;
+}
