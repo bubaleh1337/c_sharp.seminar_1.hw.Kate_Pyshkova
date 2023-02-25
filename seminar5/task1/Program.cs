f7535ab3478bfbc8b8a74c231f985a91078ca4b9
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-02-26 00:21:57 +0500

    added task1 for sem5

---
+++ seminar5/task1/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+Console.Clear();
+int[] array = GetArray();
+PrintArray(array);
+Console.Write($" -> {GetEvenNumbers(array)}");
+
+
+int[] GetArray()
+{
+  int size = new Random().Next(5, 15);
+  int[] array = new int [size];
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = new Random().Next(99, 1000);
+  }
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  Console.Write($"[{String.Join(", ", array)}]");
+}
+
+int GetEvenNumbers(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] % 2 == 0)
+    {
+      count++;
+    }
+  }
+  return count;
+}
