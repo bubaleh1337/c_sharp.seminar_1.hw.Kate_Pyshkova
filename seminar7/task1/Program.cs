2e420e1adbda4aca84aa95da3481138ad9bbe850
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-03-03 20:41:54 +0500

    added task1 for sem7 (without correct solution)

---
+++ seminar7/task1/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9 */
+
+using System;
+using static System.Console;
+
+Clear();
+int m = Prompt("Введите число m: ");
+int n = Prompt("Введите число n: ");
+double[,] array = GetArray(m, n);
+PrintArray(array);
+
+int Prompt (string message)
+{
+  Write(message);
+  int num = int.Parse(ReadLine()!);
+  return num;
+}
+
+double[,] GetArray (int m, int n)
+{
+  double[,] array = new double[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      array[i, j] = new Random().NextDouble();
+    }
+  }
+  return array;
+}
+
+void PrintArray(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Write($"{array[i, j]} ");
+    }
+    WriteLine();
+  }
+}
+
