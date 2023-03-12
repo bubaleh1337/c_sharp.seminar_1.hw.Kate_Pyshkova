b022875d0782280cb0564ad7c822c52e932bc011
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-03-12 21:13:01 +0500

    updated solution of task3 for sem8 (NOT FINISHED)

--- seminar8/task3/Program.cs
+++ seminar8/task3/Program.cs
@@ -1,5 +1,5 @@
-﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет 
-находить произведение двух матриц.
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, 
+которая будет находить произведение двух матриц.
 Например, даны 2 матрицы:
 2 4 | 3 4
 3 2 | 3 3
@@ -7,7 +7,67 @@
 18 20
 15 18 */
 
-void MultiplyMatrix(int[,] matrix)
+Console.Clear();
+int[,] matrixA = {{2, 4}, {3, 2}};
+int[,] matrixB = {{3, 4}, {3, 3}};
+PrintMatrix(matrixA, matrixB);
+MultiplyMatrix(matrixA, matrixB);
+Console.WriteLine();
+PrintResultMatrix(MultiplyMatrix(matrixA, matrixB));
+
+int[,] GetMatrix()
 {
-  int[] array = new int[matrix.GetLength(0)*matrix.GetLength(1)];
+  int[,] matrix = new int[2, 2];
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(1, 10);
+    }
+  }
+  return matrix;
+}
+
+int[,] MultiplyMatrix(int[,] matrix1, int[,] matrix2)
+{
+  int[,] result = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+  for (int i = 0; i < matrix1.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+      result[i, j] = 0;
+      for (int k = 0; k < matrix1.GetLength(1); k++)
+      {
+        result[i, j] += matrix1[i, k] * matrix2[k, j];
+      }
+    }
+  }
+  return result;
+}
+
+void PrintMatrix(int[,] a, int[,] b)
+{
+  for (int i = 0; i < a.GetLength(0) && i < b.GetLength(0); i++)
+  {
+    for (int j = 0; j < a.GetLength(1) && i < b.GetLength(1); j++)
+    {
+      if (j == a.GetLength(1)/2) Console.Write(" | ");
+      Console.Write(a[i, j] + " " + b[i, j]);
+    }
+    Console.WriteLine();
+  }
+}
+
+void PrintResultMatrix(int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      if (j == resultMatrix.GetLength(1)/2) Console.Write(" ", -2);
+      Console.Write(resultMatrix[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
 }
