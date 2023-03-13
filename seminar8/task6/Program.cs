2d147b4934110387ff9215f640ef9305e07ffa8d
bubaleh1337 <ekaterina.pyshkova@gmail.com>
2023-03-13 23:06:06 +0500

    added task6 for sem8

---
+++ seminar8/task6/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 61: Показать треугольник Паскаля.
+Сделать вывод в виде равнобедренного треугольника. */
+
+Console.Clear();
+
+Console.Write("Enter rows of Pascal triangle: ");
+int rows = int.Parse(Console.ReadLine());
+PrintTriangle(rows);
+
+void PrintTriangle(int rows)
+{
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j <= (rows - i); j++)
+    {
+      Console.Write("  ");
+    }
+    for (int k = 0; k <= i; k++)
+    {
+      Console.Write("   "); 
+      Console.Write(factorial(i) / (factorial(k) * factorial(i - k)));
+    }
+    Console.WriteLine();
+  }
+}
+
+float GetFactorial(int n)
+{
+  float result = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    result *= i;
+  }
+  return result;
+}
